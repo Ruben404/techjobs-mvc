dfb7ad7af716adae4f3119625d244ea5b8ea7f67
Ruben Lazarin <rubenlazarin03@gmail.com>
2019-06-23 14:43:34 -0500

    Working, but it doesn't look nice.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,25 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String Results(Model model, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+
+        model.addAttribute("title", "Search Results for: " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
+
+//    @RequestMapping(value = "results", method = RequestMethod.POST)
+//    public String processShowResults(@RequestParam String searchTerm) {
+//    }
 
 }
+
+
+
+//    public String search() {
+//
+//        Model name = new model
+//        name.addAttribute("columns", ListController.columnChoices);
+//    }
